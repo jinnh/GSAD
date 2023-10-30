724a8b11e3cc80bc5953bbf10ff6aee6656bc354
JinnhDev <jinhuihou@gmail.com>
2023-10-31 04:29:35 +0800

    Add files via upload

---
+++ model/model.py
@@ -0,0 +1,287 @@
+import logging
+from collections import OrderedDict
+import torch
+import torch.nn as nn
+import os
+import model.networks as networks
+from .base_model import BaseModel
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+logger = logging.getLogger('base')
+
+
+class DDPM(BaseModel):
+    def __init__(self, opt):
+        super(DDPM, self).__init__(opt)
+
+        if opt['dist']:
+            self.local_rank = torch.distributed.get_rank()
+            torch.cuda.set_device(self.local_rank)
+            device = torch.device("cuda", self.local_rank)
+        # define network and load pretrained models
+        self.netG = self.set_device(networks.define_G(opt))
+        if opt['dist']:
+            self.netG.to(device)
+       
+        # self.netG.to(device)
+        if not opt['uncertainty_train']:
+            self.netGU = self.set_device(networks.define_G(opt)) # uncertainty model
+            if opt['dist']:
+                self.netGU.to(device)
+       
+
+        self.schedule_phase = None
+        self.opt = opt
+
+        # set loss and load resume state
+        self.set_loss()
+
+        self.set_new_noise_schedule(
+            opt['model']['beta_schedule']['train'], schedule_phase='train')
+        if self.opt['phase'] == 'train':
+            self.netG.train()
+            # find the parameters to optimize
+            if opt['model']['finetune_norm']:
+                optim_params = []
+                for k, v in self.netG.named_parameters():
+                    v.requires_grad = False
+                    if k.find('transformer') >= 0:
+                        v.requires_grad = True
+                        v.data.zero_()
+                        optim_params.append(v)
+                        logger.info(
+                            'Params [{:s}] initialized to 0 and will optimize.'.format(k))
+            else:
+                optim_params = list(self.netG.parameters())
+
+            self.optG = torch.optim.Adam(
+                optim_params, lr=opt['train']["optimizer"]["lr"])
+            self.log_dict = OrderedDict()
+
+        if not opt['uncertainty_train'] and self.opt['phase'] == 'train':
+            # self.netGU.load_state_dict(torch.load(self.opt['path']['resume_state']+'_gen.pth'), strict=True)
+            if opt['dist']:
+                self.netGU = DDP(self.netGU, device_ids=[self.local_rank], output_device=self.local_rank, find_unused_parameters=True)
+
+        if self.opt['phase'] == 'test':
+            try:
+                self.netG.load_state_dict(torch.load(self.opt['path']['resume_state']), strict=True)
+            except Exception:
+                self.netG = nn.DataParallel(self.netGU)
+                self.netG.load_state_dict(torch.load(self.opt['path']['resume_state']), strict=True)
+        else:
+            self.load_network()
+            if opt['dist']:
+                self.netG = DDP(self.netG, device_ids=[self.local_rank], output_device=self.local_rank,find_unused_parameters=True)
+        self.print_network()
+
+    def feed_data(self, data):
+
+        dic = {}
+
+        if self.opt['dist']:
+            dic = {}
+            dic['LQ'] = data['LQ'].to(self.local_rank)
+            dic['GT'] = data['GT'].to(self.local_rank)
+            self.data = dic
+        else:
+            dic['LQ'] = data['LQ']
+            dic['GT'] = data['GT']
+
+            self.data = self.set_device(dic)
+
+    def optimize_parameters(self):
+
+        self.optG.zero_grad()
+
+        if not self.opt['uncertainty_train']:
+            if self.opt['dist']:
+                l_pix, l_gsad = self.netG(self.data, self.netGU.module.denoise_fn)
+            else:
+                l_pix, l_gsad = self.netG(self.data, self.netGU.denoise_fn)
+
+            # need to average in multi-gpu
+            b, c, h, w = self.data['LQ'].shape
+
+            num_clusters = 6
+            l_pix = l_pix.sum()/int(b*c*h*w)
+            l_gsad = l_gsad.sum()/int(b*num_clusters)
+            # l_svd = l_svd.sum()/int(b*c*h*w)
+            loss = l_pix + l_gsad
+            loss.backward()
+            self.optG.step()
+
+            # set log
+            self.log_dict['total_loss'] = loss.item()
+            self.log_dict['l_1'] = l_pix.item()
+            self.log_dict['l_gsad'] = l_gsad.item()
+        else:
+            l_pix = self.netG(self.data)
+
+            b, c, h, w = self.data['LQ'].shape
+
+            l_pix = l_pix.sum()/int(b*c*h*w)
+            l_pix.backward()
+            self.optG.step()
+
+            # set log
+            self.log_dict['l_u'] = l_pix.item()
+
+
+    def test(self, continous=False):
+        self.netG.eval()
+        with torch.no_grad():
+            if isinstance(self.netG, nn.DataParallel):
+                self.SR = self.netG.module.super_resolution(
+                    self.data['LQ'], continous)
+                
+            else:
+                if self.opt['dist']:
+                    self.SR = self.netG.module.super_resolution(self.data['LQ'], continous)
+                else:
+                    self.SR = self.netG.super_resolution(self.data['LQ'], continous)
+
+        self.netG.train()
+
+    def sample(self, batch_size=1, continous=False):
+        self.netG.eval()
+        with torch.no_grad():
+            if isinstance(self.netG, nn.DataParallel):
+                self.SR = self.netG.module.sample(batch_size, continous)
+            else:
+                self.SR = self.netG.sample(batch_size, continous)
+        self.netG.train()
+
+    def set_loss(self):
+        if isinstance(self.netG, nn.DataParallel):
+            self.netG.module.set_loss(self.device)
+        else:
+            self.netG.set_loss(self.device)
+
+        if not self.opt['uncertainty_train']:
+            if isinstance(self.netGU, nn.DataParallel):
+                self.netGU.module.set_loss(self.device)
+            else:
+                self.netGU.set_loss(self.device)
+
+    def set_new_noise_schedule(self, schedule_opt, schedule_phase='train'):
+
+        if self.opt['dist']:
+            # local_rank = torch.distributed.get_rank()
+            device = torch.device("cuda", self.local_rank)
+            if self.schedule_phase is None or self.schedule_phase != schedule_phase:
+                self.schedule_phase = schedule_phase
+                if isinstance(self.netG, nn.DataParallel):
+                    self.netG.module.set_new_noise_schedule(
+                        schedule_opt, self.device)
+                else:
+                    self.netG.set_new_noise_schedule(schedule_opt, device)
+
+                if not self.opt['uncertainty_train']:
+                    if isinstance(self.netGU, nn.DataParallel):
+                        self.netGU.module.set_new_noise_schedule(
+                            schedule_opt, self.device)
+                    else:
+                        self.netGU.set_new_noise_schedule(schedule_opt, device)
+        else:
+            self.schedule_phase = schedule_phase
+            if isinstance(self.netG, nn.DataParallel):
+                self.netG.module.set_new_noise_schedule(
+                    schedule_opt, self.device)
+            else:
+                self.netG.set_new_noise_schedule(schedule_opt, self.device)
+
+            if not self.opt['uncertainty_train']:
+                if isinstance(self.netGU, nn.DataParallel):
+                    self.netGU.module.set_new_noise_schedule(
+                        schedule_opt, self.device)
+                else:
+                    self.netGU.set_new_noise_schedule(schedule_opt, self.device)
+
+    def get_current_log(self):
+        return self.log_dict
+
+    def get_current_visuals(self, need_LR=True, sample=False):
+        out_dict = OrderedDict()
+        if sample:
+            out_dict['SAM'] = self.SR.detach().float().cpu()
+        else:
+            out_dict['HQ'] = self.SR.detach().float().cpu()
+            out_dict['INF'] = self.data['LQ'].detach().float().cpu()
+            out_dict['GT'] = self.data['GT'].detach()[0].float().cpu()
+            if need_LR and 'LR' in self.data:
+                out_dict['LQ'] = self.data['LQ'].detach().float().cpu()
+            else:
+                out_dict['LQ'] = out_dict['INF']
+        return out_dict
+
+    def print_network(self):
+        s, n = self.get_network_description(self.netG)
+        if isinstance(self.netG, nn.DataParallel):
+            net_struc_str = '{} - {}'.format(self.netG.__class__.__name__,
+                                             self.netG.module.__class__.__name__)
+        else:
+            net_struc_str = '{}'.format(self.netG.__class__.__name__)
+
+        logger.info(s)
+
+        logger.info(
+            'Network G structure: {}, with parameters: {:,d}'.format(net_struc_str, n))
+
+    def save_network(self, epoch, iter_step):
+        gen_path = os.path.join(
+            self.opt['path']['checkpoint'], 'I{}_E{}_gen.pth'.format(iter_step, epoch))
+        opt_path = os.path.join(
+            self.opt['path']['checkpoint'], 'I{}_E{}_opt.pth'.format(iter_step, epoch))
+        
+        # gen
+        network = self.netG
+        if isinstance(self.netG, nn.DataParallel):
+            network = network.module
+        state_dict = network.state_dict()
+        for key, param in state_dict.items():
+            state_dict[key] = param.cpu()
+        torch.save(state_dict, gen_path)
+        # opt
+        opt_state = {'epoch': epoch, 'iter': iter_step,
+                     'scheduler': None, 'optimizer': None}
+        opt_state['optimizer'] = self.optG.state_dict()
+        torch.save(opt_state, opt_path)
+
+        if self.opt['uncertainty_train']:
+            ut_gen_path = os.path.join(
+                './checkpoints/uncertainty/', 'latest_gen.pth'.format(iter_step, epoch))
+            ut_opt_path = os.path.join(
+                './checkpoints/uncertainty/', 'latest_opt.pth'.format(iter_step, epoch))
+            torch.save(state_dict, ut_gen_path)
+            torch.save(opt_state, ut_opt_path)
+
+        logger.info(
+            'Saved model in [{:s}] ...'.format(gen_path))
+
+    def load_network(self):
+        load_path = self.opt['path']['resume_state']
+        if load_path is not None:
+            logger.info(
+                'Loading pretrained model for G [{:s}] ...'.format(load_path))
+            gen_path = '{}_gen.pth'.format(load_path)
+            opt_path = '{}_opt.pth'.format(load_path)
+            # gen
+            network = self.netG
+            if isinstance(self.netG, nn.DataParallel):
+                network = network.module
+
+            # network = nn.DataParallel(network).cuda()
+
+            network.load_state_dict(torch.load(
+                gen_path), strict=(not self.opt['model']['finetune_norm']))
+            # network.load_state_dict(torch.load(
+            #     gen_path), strict=False)
+            if self.opt['phase'] == 'train':
+                # optimizer
+                opt = torch.load(opt_path)
+                self.optG.load_state_dict(opt['optimizer'])
+                # self.begin_step = opt['iter']
+                # self.begin_epoch = opt['epoch']
+                self.begin_step = 0
+                self.begin_epoch = 0
---
+++ model/networks.py
@@ -0,0 +1,161 @@
+import functools
+import logging
+import torch
+import torch.nn as nn
+from torch.nn import init
+from torch.nn import modules
+logger = logging.getLogger('base')
+####################
+# initialize
+####################
+
+
+def weights_init_normal(m, std=0.02):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        init.normal_(m.weight.data, 0.0, std)
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif classname.find('Linear') != -1:
+        init.normal_(m.weight.data, 0.0, std)
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif classname.find('BatchNorm2d') != -1:
+        init.normal_(m.weight.data, 1.0, std)  # BN also uses norm
+        init.constant_(m.bias.data, 0.0)
+
+
+def weights_init_kaiming(m, scale=1):
+    classname = m.__class__.__name__
+    if classname.find('Conv2d') != -1:
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+        m.weight.data *= scale
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif classname.find('Linear') != -1:
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+        m.weight.data *= scale
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif classname.find('BatchNorm2d') != -1:
+        init.constant_(m.weight.data, 1.0)
+        init.constant_(m.bias.data, 0.0)
+
+
+def weights_init_orthogonal(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        init.orthogonal_(m.weight.data, gain=1)
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif classname.find('Linear') != -1:
+        init.orthogonal_(m.weight.data, gain=1)
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif classname.find('BatchNorm2d') != -1:
+        init.constant_(m.weight.data, 1.0)
+        init.constant_(m.bias.data, 0.0)
+
+
+def init_weights(net, init_type='kaiming', scale=1, std=0.02):
+    # scale for 'kaiming', std for 'normal'.
+    logger.info('Initialization method [{:s}]'.format(init_type))
+    if init_type == 'normal':
+        weights_init_normal_ = functools.partial(weights_init_normal, std=std)
+        net.apply(weights_init_normal_)
+    elif init_type == 'kaiming':
+        weights_init_kaiming_ = functools.partial(
+            weights_init_kaiming, scale=scale)
+        net.apply(weights_init_kaiming_)
+    elif init_type == 'orthogonal':
+        net.apply(weights_init_orthogonal)
+    else:
+        raise NotImplementedError(
+            'initialization method [{:s}] not implemented'.format(init_type))
+
+
+####################
+# define network
+####################
+
+
+# Generator
+def define_G(opt):
+    model_opt = opt['model']
+    print(model_opt['which_model_G'])
+    if model_opt['which_model_G'] == 'ddpm':
+        from .ddpm_modules import diffusion, unet, diffusion_Pt
+    if ('norm_groups' not in model_opt['unet']) or model_opt['unet']['norm_groups'] is None:
+        model_opt['unet']['norm_groups']=32
+    model = unet.UNet(
+        in_channel=model_opt['unet']['in_channel'],
+        out_channel=model_opt['unet']['out_channel'],
+        norm_groups=model_opt['unet']['norm_groups'],
+        inner_channel=model_opt['unet']['inner_channel'],
+        channel_mults=model_opt['unet']['channel_multiplier'],
+        attn_res=model_opt['unet']['attn_res'],
+        res_blocks=model_opt['unet']['res_blocks'],
+        dropout=model_opt['unet']['dropout'],
+        image_size=model_opt['diffusion']['image_size']
+    )
+    if opt['uncertainty_train']:
+        netG = diffusion_Pt.GaussianDiffusion(
+            model,
+            image_size=model_opt['diffusion']['image_size'],
+            channels=model_opt['diffusion']['channels'],
+            loss_type='l1',   
+            conditional=model_opt['diffusion']['conditional'],
+            schedule_opt=model_opt['beta_schedule']['train']
+        )
+    else:
+        netG = diffusion.GaussianDiffusion(
+            model,
+            image_size=model_opt['diffusion']['image_size'],
+            channels=model_opt['diffusion']['channels'],
+            loss_type='l1',   
+            conditional=model_opt['diffusion']['conditional'],
+            schedule_opt=model_opt['beta_schedule']['train']
+        )
+    if opt['phase'] == 'train':
+        # init_weights(netG, init_type='kaiming', scale=0.1)
+        init_weights(netG, init_type='orthogonal')
+    if opt['gpu_ids'] and opt['distributed']:
+        assert torch.cuda.is_available()
+        netG = nn.DataParallel(netG)
+    return netG
+
+    
+# Generator
+def define_GGG(opt):
+    model_opt = opt['model']
+    print(model_opt['which_model_G'])
+    if model_opt['which_model_G'] == 'ddpm':
+        from .ddpm_modules import diffusion, unet
+    if ('norm_groups' not in model_opt['unet']) or model_opt['unet']['norm_groups'] is None:
+        model_opt['unet']['norm_groups']=32
+    model = unet.UNet(
+        in_channel=model_opt['unet']['in_channel'],
+        out_channel=model_opt['unet']['out_channel'],
+        norm_groups=model_opt['unet']['norm_groups'],
+        inner_channel=model_opt['unet']['inner_channel'],
+        channel_mults=model_opt['unet']['channel_multiplier'],
+        attn_res=model_opt['unet']['attn_res'],
+        res_blocks=model_opt['unet']['res_blocks'],
+        dropout=model_opt['unet']['dropout'],
+        image_size=model_opt['diffusion']['image_size']
+    )
+    netGVar = diffusion.GaussianDiffusion(
+        model,
+        image_size=model_opt['diffusion']['image_size'],
+        channels=model_opt['diffusion']['channels'],
+        loss_type='l1',  
+        conditional=model_opt['diffusion']['conditional'],
+        schedule_opt=model_opt['beta_schedule']['train']
+    )
+    if opt['phase'] == 'train':
+        # init_weights(netG, init_type='kaiming', scale=0.1)
+        init_weights(netGVar, init_type='orthogonal')
+    if opt['gpu_ids'] and opt['distributed']:
+        assert torch.cuda.is_available()
+        netGVar = nn.DataParallel(netGVar)
+    return netGVar
